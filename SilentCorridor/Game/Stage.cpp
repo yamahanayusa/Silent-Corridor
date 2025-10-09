72e03ebad562fda81f6801b4680a0e3c6f34aade
yamahanayusa <CA01244031@st.kawahara.ac.jp>
2025-10-09 19:27:51 +0900

    Stageの調整

--- SilentCorridor/Game/Stage.cpp
+++ SilentCorridor/Game/Stage.cpp
@@ -3,7 +3,7 @@
 
 Stage::Stage()
 {
-	m_modelRender.Init("Assets/modelData/stage1.tkm");
+	m_modelRender.Init("Assets/modelData/stage.tkm");
 	m_modelRender.Update();
 	physicsStaticObject.CreateFromModel(m_modelRender.GetModel(), m_modelRender.GetModel().GetWorldMatrix());
 
