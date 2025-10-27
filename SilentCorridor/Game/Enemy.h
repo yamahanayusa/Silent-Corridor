bc918ec750a4f4daa64ff0e1f8d5179003afa7f5
YamahanaYusa <167502245+yamahanayusa@users.noreply.github.com>
2025-10-27 19:56:09 +0900

    ウェイポイントの追加 (#10)

--- SilentCorridor/Game/Enemy.h
+++ SilentCorridor/Game/Enemy.h
@@ -4,6 +4,13 @@
 #include "AI/PathFinding/Path.h"
 #include "AI/PathFinding/NaviMesh.h"
 
+enum Animation
+{
+    enAnimationClip_Walk,
+    enAnimationClip_Idle,
+    enAnimationClip_Num
+};
+
 class Stage;
 class Enemy : public Character
 {
@@ -13,14 +20,25 @@ public:
     void Render(RenderContext& rc) override;
 
 private:
+    void InitPatrolPoints(); // ウェイポイントの定義専用
+    void UpdateMove(); // 移動と実行とゴール判定
+    void FindNextPatrolTarget(); // ランダム目標の決定と経路探索
+    void UpdateAnimation(); // アニメーション
+
+private:
     nsAI::NaviMesh* m_navMesh = nullptr; // ナビメッシュ
     nsAI::PathFinding m_pathFinder; // 経路探索
     nsAI::Path m_path; // 経路データ
     Stage* m_stage = nullptr;
 
+    std::vector<Vector3>m_patrolPoints; // 徘徊ポイントリスト
     Vector3 m_targetPos = Vector3::Zero; // 移動目標
 
+    bool m_isMoving = true;
     float m_speed = 10.0f; // 移動速度
+    int m_patrolIndex = -1; // 現在の目標ポイントのインデックス
 
     ModelRender m_modelRender;
+    AnimationClip m_animationClips[enAnimationClip_Num];		//アニメーションクリップ。
+
 };
--- SilentCorridor/Game/Player.h
+++ SilentCorridor/Game/Player.h
@@ -12,9 +12,10 @@ public:
     bool Start() override;     // 初期化処理
     void Update() override;    // 更新処理
 
-
-    Vector3	m_forward = Vector3::AxisZ; //ライト用
-
 private:
     void HandleInput();   // 入力処理
+
+public:
+    Vector3	m_forward = Vector3::AxisZ; //ライト用
+
 };
