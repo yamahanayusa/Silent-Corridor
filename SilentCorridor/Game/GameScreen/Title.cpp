9cb430f52c5505cfb0d619382272c31499010880
YamahanaYusa <167502245+yamahanayusa@users.noreply.github.com>
2025-12-16 09:59:20 +0900

    タイトルの追加 (#17)

---
+++ SilentCorridor/Game/GameScreen/Title.cpp
@@ -0,0 +1,25 @@
+#include "stdafx.h"
+#include "Title.h"
+#include "Game.h"
+
+bool Title::Start()
+{
+	m_spriteRender.Init("Assets/sprite/Title.DDS", 1920.0f, 1080.0f);
+
+	return true;
+}
+
+void Title::Update()
+{
+	//Aボタンが押されたら新しいゲームを始める。
+	if (g_pad[0]->IsTrigger(enButtonA))
+	{
+		NewGO<Game>(0, "game");
+		DeleteGO(this);
+	}
+}
+
+void Title::Render(RenderContext& rc)
+{
+	m_spriteRender.Draw(rc);
+}
--- SilentCorridor/Game/main.cpp
+++ SilentCorridor/Game/main.cpp
@@ -1,6 +1,7 @@
 #include "stdafx.h"
 #include "system/system.h"
 #include "Game.h"
+#include "GameScreen/Title.h"
 
 // K2EngineLowのグローバルアクセスポイント。
 K2EngineLow* g_k2EngineLow = nullptr;
@@ -18,7 +19,8 @@ int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPWSTR lpCmdLi
 	g_k2EngineLow->Init(g_hWnd, FRAME_BUFFER_W, FRAME_BUFFER_H);
 	g_camera3D->SetPosition({ 0.0f, 100.0f, -200.0f });
 	g_camera3D->SetTarget({ 0.0f, 50.0f, 0.0f });
-	NewGO<Game>(0, "game");
+	//NewGO<Game>(0, "game");
+	NewGO<Title>(0, "title");
 
 	// 視点の位置を設定する
 	
