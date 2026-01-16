02ee02254434fd055361ef7b7f2de01edd2107e8
YamahanaYusa <167502245+yamahanayusa@users.noreply.github.com>
2026-01-16 11:48:13 +0900

    TitleにUIを追加とGameOverにコメントを追加 (#21)

--- SilentCorridor/Game/GameScreen/GameOver.h
+++ SilentCorridor/Game/GameScreen/GameOver.h
@@ -10,9 +10,9 @@ public:
 	void Render(RenderContext& rc)override;
 
 private:
-	void UpdateSelection();
-	void CheckConfirmation();
-	void UpdateVisuals();
+	void UpdateSelection(); // パッドの入力をチェックして、選択項目(Index)を更新する
+	void CheckConfirmation(); // 決定ボタンが押されたかチェックする
+	void UpdateVisuals(); // 選択状態に合わせて、文字の色や大きさを更新する
 private:
 	int m_selectIndex = 0;	// メニューに戻るかリスタートするかの選択の際に使用
 
